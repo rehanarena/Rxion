b78c544271fd1e06bec80ebd7b06e708f2e29891
Rehana <150910796+rehanarena@users.noreply.github.com>
2025-04-28 20:28:21 +0530

    Update doctorRoute.js

--- backend/dist/routes/doctorRoute.js
+++ backend/dist/routes/doctorRoute.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const multer_1 = __importDefault(require("../middlewares/multer"));
 const DoctorRepository_1 = require("../repositories/doctor/doctorRepository");
-const doctorService_1 = require("../services/doctor/doctorService");
+const doctorService_1 = require("../services/doctor/DoctorService");
 const doctorController_1 = require("../controllers/doctor/doctorController");
 const authDoctor_1 = __importDefault(require("../middlewares/authDoctor"));
 const doctorOTPRepository_1 = require("../repositories/doctor/doctorOTPRepository");
